560773b15fa82340e06bc3f8d5e3e53180067d9d
Mirjax2000 <miroslav.viktorin@seznam.cz>
2023-03-15 21:09:49 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/** @format */
+
+let form = document.getElementById('form'),
+    button = document.getElementById('button');
+console.log('ahoj');
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+});
